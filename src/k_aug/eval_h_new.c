550ec539d335a24d7a90b6ece21679a663902852
dthierry <dmolinat@andrew.cheme.cmu.edu>
2018-08-20 10:40:14 -0400

    new evaluation schemes

---
+++ src/k_aug/eval_h_new.c
@@ -0,0 +1,8 @@
+//
+// Created by dav0 on 7/31/18.
+//
+
+#include "eval_h_new.h"
+
+
+int eval_new()
---
+++ src/k_aug/eval_h_new.h
@@ -0,0 +1,8 @@
+//
+// Created by dav0 on 7/31/18.
+//
+
+#ifndef K_AUG_EVAL_H_NEW_H
+#define K_AUG_EVAL_H_NEW_H
+
+#endif //K_AUG_EVAL_H_NEW_H
