a4d17549c687bbd7b3d70bcf30939897579ed127
dthierry <dmolinat@andrew.cmu.edu>
2019-06-15 18:49:21 -0400

    test svd deb_kkt

--- src/matrix/dgesdd_driver.c
+++ src/matrix/dgesdd_driver.c
@@ -22,6 +22,7 @@ void dgesdd_driver(fint n, fint m, const double *a, fint nza, const fint *arow, 
     int lwork = 3 * MIN(m, n) + MAX(MAX(m, n), 7 * MIN(m, n));
     int info = 0;
     int *iwork;
+    FILE *whatevs;
     /* to be safe ... */
     lwork = lwork * 2;
     a_dense = (double *) malloc(sizeof(double) * d_size);
@@ -33,9 +34,39 @@ void dgesdd_driver(fint n, fint m, const double *a, fint nza, const fint *arow, 
     assert(work);
     iwork = (int *) malloc(sizeof(int) * 8 * MIN(m, n));
     assert(iwork);
+    /*
+     * fortran row and column indexing, starting at 1 */
+    /*
+    * whatevs = fopen("sparse_.txt", "w");
+    * for (i = 0; i < nza; i++) {fprintf(whatevs,"a[%d, %d] = %f\n", arow[i], acol[i], a[i]);}
+    * fclose(whatevs);
+     */
+
     for (i = 0; i < nza; i++) {
-        a_dense[arow[i] + acol[i] * m] = a[i];
+        a_dense[(arow[i] - 1) + (acol[i] - 1) * m] = a[i];
+    }
+    /*
+    * whatevs = fopen("dense_.txt", "w");
+    * for(i = 0; i<n; i++){
+    *    for(j = 0; j<m; j++){
+    *        fprintf(whatevs, "a[%d, %d] = %f\n", i, j, a_dense[i + j * m]);
+    *    }
+    }
+     */
+
+    dgesdd_(&job, &m, &n, a_dense, &m, s, NULL, &m, NULL, &m, work, &lwork, iwork, &info);
+
+    printf("info %d\n\n", info);
+    if (info == 0){
+        whatevs = fopen("singular_values_.txt", "w");
+        for(i = 0; i < MIN(m,n); i++){
+            fprintf(whatevs, "%.g\n", s[i]);
+        }
+        fclose(whatevs);
     }
 
-
+    free(a_dense);
+    free(s);
+    free(work);
+    free(iwork);
 }
